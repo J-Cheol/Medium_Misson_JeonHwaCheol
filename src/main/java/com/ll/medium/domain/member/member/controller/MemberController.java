b634778b16b4a81b988f77431f030d3bb66e4189
J-Cheol <lunaris0206@naver.com>
2023-12-12 17:14:06 +0900

    UTF-8

--- src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
@@ -1,20 +1,25 @@
 package com.ll.medium.domain.member.member.controller;
 
-import com.ll.medium.domain.member.member.repository.MemberRepository;
+import com.ll.medium.domain.member.member.entity.Member;
+import com.ll.medium.domain.member.member.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.apache.catalina.util.URLEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.nio.charset.StandardCharsets;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/member")
 public class MemberController
 {
-    private final MemberRepository memberRepository;
+    private final MemberService memberService;
 
     @Getter
     @Setter
@@ -26,9 +31,12 @@ public class MemberController
 
 
     @PostMapping("/join")
-    String showJoin(JoinForm joinForm)
+    String showJoin(@Valid JoinForm joinForm)
     {
-        return "redirect:/";
+        Member member = memberService.join(joinForm.getUsername(), joinForm.getPassword());
+        String msg = "%s님 환영합니다. 회원가입이 완료되었습니다. 로그인 후 이용해주세요.".formatted(member.getUsername());
+        msg = new URLEncoder().encode(msg, StandardCharsets.UTF_8);
+        return "redirect:/?msg=" + msg;
     }
 
     @GetMapping("/join")
