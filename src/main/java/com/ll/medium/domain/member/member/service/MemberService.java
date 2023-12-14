7d353e2a9ba6d079b584542a40786cfc35fb6f80
J-Cheol <lunaris0206@naver.com>
2023-12-14 14:21:45 +0900

    login

--- src/main/java/com/ll/medium/domain/member/member/service/MemberService.java
+++ src/main/java/com/ll/medium/domain/member/member/service/MemberService.java
@@ -4,6 +4,7 @@ import com.ll.medium.domain.member.member.entity.Member;
 import com.ll.medium.domain.member.member.repository.MemberRepository;
 import com.ll.medium.global.rsData.RsData.RsData;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,10 +17,13 @@ import java.util.Optional;
 public class MemberService
 {
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
-    public RsData<Member> join(String username, String password) {
-        if (List.of("admin", "system").contains(username)) {
+    public RsData<Member> join(String username, String password)
+    {
+        if (List.of("admin", "system").contains(username))
+        {
             return RsData.of("400-1", "%s(은)는 사용할 수 없는 아이디 입니다.".formatted(username));
         }
         if (findByUsername(username).isPresent())
@@ -27,7 +31,7 @@ public class MemberService
             return RsData.of("400-2", "이미 존재하는 회원입니다.");
         }
 
-        Member member = new Member(username, password);
+        Member member = new Member(username, passwordEncoder.encode(password));
         memberRepository.save(member);
 
         return RsData.of("200", "%s님 환영합니다. 회원가입이 완료되었습니다. 로그인 후 이용해주세요.".formatted(member.getUsername()), member);
--- src/main/java/com/ll/medium/global/security/SecurityConfig.java
+++ src/main/java/com/ll/medium/global/security/SecurityConfig.java
@@ -3,15 +3,25 @@ package com.ll.medium.global.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig
+{
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
+    {
         http
                 .authorizeRequests(authorizeRequests ->
                         authorizeRequests.requestMatchers("/**")
